f390a553742b920931faae2e9f09b3618a49f3d1
JCMLG <jcordeiromlg@gmail.com>
2020-11-04 22:13:12 -0500

    Uploading Credits and Main Menu Scene, Editor Build settings include these scenes
    Credits scene returns the player to the main menu
    Main Menu Scene has 4 buttons

---
+++ Assets/Jose/Scripts/MainMenu/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+   
+    //From Jose: This is my function to quit the game, after user clicks quit game button, the game will close.
+    public void Quitgame()
+    {
+        Debug.Log("the player has quit the application. closing application");
+        Application.Quit();
+    }
+
+}
